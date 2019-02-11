447828b1d91a8a6f653b1af6101b00b19ebea2ae
19revey <duanyifeixp@gmail.com>
2019-02-11 15:45:33 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #echo "Please input compute capabiliy of your GPU, (52 for example), otherwise 53 is the default value"
 #read varname
-#if [$varname -lt 53] && [$varname -gt 50]
+#if [$varname -gt 50]
 #then 
 #	sed -i 's/53/$varname/g' src/CMakeLists.txt
 #else
